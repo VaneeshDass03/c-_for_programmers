c5778a8953b3e708adc483f53624cca2a58aa554
Vaneesh Dass <vaneesh.dass03@gmail.com>
2019-01-23 17:39:22 +0530

    C++ support 3 logical operators. Demo of all 3's.

--- main.cpp
+++ main.cpp
@@ -1,37 +1,37 @@
+/*Goal: understand the logical operators supported by C++
+*/
+
 #include<iostream>
 
 int main() {
+    int A = 5;
+    int B = 4;
+    int C = 5;
+    int D = 0;
 
-    int a = 65;
-    char charA = 65;
-    char charB = 'B';
-    float answer = 0;
-    char charC = 67;
-    int integer = 80;
-    float floatNumber = 0.0;
+    std::string TorF[] = {"False", "True"};
 
-    std::cout << "a = " << a << "\n";
-    std::cout << "charA = " << charA << "\n";
-    std::cout << "charB = " << charB << "\n";
+    //The && operator
+    std::cout << "A == C is " << TorF[A == C];
+    std::cout << "\n(B == D) is " << TorF[B == D];
+    std::cout << "\n(B > D) is " << TorF[B > D];
+    //A true && false = false
+    std::cout << "\n\n(A ==C) && (B == D) is " << TorF[(A == C) && (B == D)];
+    //A true and true = true
+    std::cout << "\n(A ==C) && (B > D) is " << TorF[(A == C) && (B > D)];
 
-    //we can assign an integer to a float
-    floatNumber = integer;
-    std::cout << "integer = " << integer << "\n\n";
-    std::cout << "floatNumber = integer = " << floatNumber << "\n";
+    //The || operator
+    //A true || false = true
+    std::cout << "\n\n(A ==C) || (B == D) is " << TorF[(A == C) || (B == D)];
+    //A true || true = true
+    std::cout << "\n(A ==C) || (B > D) is " << TorF[(A == C) || (B > D)];
 
-    //we can assign a char to a float
-    floatNumber = charB;
-    std::cout << "floatNumber = charB = " << floatNumber << "\n";
+    //The 'Not' operator
+    std::cout << "\n\nA < B is " << TorF[A < B];
+    std::cout << "\n!(A < B) is " << TorF[!(A < B)];
 
-    answer = floatNumber / 4;
-    std::cout << "answer = floatNumber/4 = " << answer << "\n";
+    std::cout << "\n\nA == C is " << TorF[A == C];
+    std::cout << "\n!(A == C) is " << TorF[!(A == C)];
 
-    //But assigning a float to a char doesn't quite work
-    charC = answer;
-    std::cout << "charC = answer = " << charC << "\n";
-
-    //assigning a float to an interger, results in the float being truncated
-    integer = answer;
-    std::cout << "integer = floatNumber = " << integer << "\n";
     return 0;
 }
