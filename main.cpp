3c8d8441d0c06d5dfe4b0445d8b244aff9a783e4
Vaneesh Dass <vaneesh.dass03@gmail.com>
2019-01-24 18:19:16 +0530

    Pointers basic demos!!

--- main.cpp
+++ main.cpp
@@ -1,8 +1,92 @@
-#include <iostream>
+/*Goal: Find out why you care about pointers*/
+
+#include<iostream>
+
+int demoOne();
+
+int demoTwo();
 
 int main() {
-    int a = 54;
-    std::cout << "a = " << a << "\n";
-    std::cout << "address of a is at &a = " << &a << "\n";
+    std::cout << "demo one\n";
+    demoOne();
+    std::cout << "demo two\n";
+    demoTwo();
+    return 0;
+}
+
+int demoTwo() {
+    std::__cxx11::string name;
+    int givenInt;
+    float givenFloat;
+    double givenDouble;
+    std::__cxx11::string givenString;
+    char givenChar;
+    int *pointerGivenInt;
+    int **pointerPointerGivenInt;
+
+    pointerGivenInt = &givenInt;
+    pointerPointerGivenInt = &pointerGivenInt;
+
+    //Get the values of each variable
+    std::cout << "integer = \n";
+    std::cin >> givenInt;
+    std::cout << "float = \n";
+    std::cin >> givenFloat;
+    std::cout << "double = \n";
+    std::cin >> givenDouble;
+    //We need to use cin.ignore so cin will ignore
+//the characters in the buffer leftover
+//from the givenDouble
+    std::cin.ignore();
+    std::cout << "character = \n";
+    std::cin >> givenChar;
+
+    std::cout << "string = \n";
+    std::cin.ignore();
+    getline(std::cin, givenString);
+
+
+    //The value stored in the data
+    std::cout << "integer = " << givenInt << "\n";
+    std::cout << "float = " << givenFloat << "\n";
+    std::cout << "double = " << givenDouble << "\n";
+    std::cout << "string = " << givenString << "\n";
+    std::cout << "character = " << (char) givenChar << "\n\n";
+
+    //The address of the data - use pointers
+    std::cout << "address integer = " << &givenInt << "\n";
+    std::cout << "address float = " << &givenFloat << "\n";
+    std::cout << "address double = " << &givenDouble << "\n";
+    std::cout << "address string = " << &givenString << "\n";
+    std::cout << "address character = " << (void *) &givenChar << "\n\n";
+
+    //Use indirection to the get the value stored at the address
+    std::cout << "pointer of givenInt = " << *pointerGivenInt << "\n";
+    std::cout << "pointer of pointer of givenInt = " << **pointerPointerGivenInt << "\n";
+
+    return 0;
+}
+
+int demoOne() {
+    int *pointerI;
+    int number;
+    char character;
+    char *pointerC;
+    std::__cxx11::string sentence;
+    std::__cxx11::string *pointerS;
+
+    pointerI = &number;
+    *pointerI = 45;
+
+    pointerC = &character;
+    *pointerC = 'f';
+
+    pointerS = &sentence;
+    *pointerS = "Hey look at me, I know pointers!";
+
+    std::cout << "number = " << number << "\n";
+    std::cout << "character = " << character << "\n";
+    std::cout << "sentence = " << sentence << "\n";
+
     return 0;
 }
