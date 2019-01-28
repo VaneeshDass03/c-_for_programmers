a47c76ff3dc02af4e8edbf8f8ce053a0ca585b51
Vaneesh Dass <vaneesh.dass03@gmail.com>
2019-01-28 16:45:25 +0530

    Pass arrays to functions using reference.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,47 @@
-#include<iostream>
+/*Goal: Learn to pass arrays to functions*/
 
-void increment(int &input); //Note the addition of '&'
+#include<iostream>
+#include<iomanip>
+
+//Pass the array as a pointer
+void arrayAsPointer(int *array, int size);
+
+//Pass the array as a sized array
+void arraySized(int array[3], int size);
+
+//Pass the array as an unsized array
+void arrayUnSized(int array[], int size);
 
 int main() {
-    int a = 34;
-    std::cout << "Before the function call a = " << a << "\n";
-    increment(a);
-    std::cout << "After the function call a = " << a << "\n";
+    const int size = 3;
+    int array[size] = {33, 66, 99};
+    //We are passing a pointer or reference to the array
+    //so we will not know the size of the array
+    //We have to pass the size to the function as well
+    arrayAsPointer(array, size);
+    arraySized(array, size);
+    arrayUnSized(array, size);
     return 0;
 }
 
-void increment(int &input)//Note the addition of '&'
-{
-    input++; //**Note the LACK OF THE addition of '&'**
-    std::cout << "In the function call a = " << input << "\n";
+void arrayAsPointer(int *array, int size) {
+    std::cout << std::setw(5);
+    for (int i = 0; i < size; i++)
+        std::cout << array[i] << " ";
+    std::cout << "\n";
 }
+
+void arraySized(int array[3], int size) {
+    std::cout << std::setw(5);
+    for (int i = 0; i < size; i++)
+        std::cout << array[i] << " ";
+    std::cout << "\n";
+}
+
+void arrayUnSized(int array[], int size) {
+    std::cout << std::setw(5);
+    for (int i = 0; i < size; i++)
+        std::cout << array[i] << " ";
+    std::cout << "\n";
+}
+
