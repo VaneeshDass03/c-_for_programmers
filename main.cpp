5ddcc5b3fb6e63605f28844161f1313e47bba721
Vaneesh Dass <vaneesh.dass03@gmail.com>
2019-01-28 15:45:27 +0530

    Function demo passing value by reference(pointer).

--- main.cpp
+++ main.cpp
@@ -1,40 +1,17 @@
-/*Goal: Find out why you care about pointers*/
-
 #include<iostream>
 
+void increment(int &input); //Note the addition of '&'
 
 int main() {
-    int userInput[40];
-
-    //Enter the numbers into an array called userInput
-    for (int i = 0; i < 40; i++) {
-        scanf("%d", &userInput[i]);
-    }
-    //print the array
-    std::cout << "\nThe array\n";
-    for (int i = 0; i < 40; i++) {
-        std::cout << userInput[i] << " ";
-    }
-    //print the array in reverse order
-    std::cout << "\n\nThe array in reverse order\n";
-    for (int i = 39; i >= 0; i--) {
-        std::cout << userInput[i] << " ";
-    }
-    //sorting the array
-    for (int i = 0; i < 40; i++) {
-        for (int j = 0; j < 39 - i; j++) {
-            if (userInput[j] > userInput[j + 1]) {
-                int temp;
-                temp = userInput[j];
-                userInput[j] = userInput[j + 1];
-                userInput[j + 1] = temp;
-            }
-        }
-    }
-    std::cout << "\n\nThe array sorted\n";
-    for (int i = 0; i < 40; i++) {
-        std::cout << userInput[i] << " ";
-    }
+    int a = 34;
+    std::cout << "Before the function call a = " << a << "\n";
+    increment(a);
+    std::cout << "After the function call a = " << a << "\n";
     return 0;
 }
 
+void increment(int &input)//Note the addition of '&'
+{
+    input++; //**Note the LACK OF THE addition of '&'**
+    std::cout << "In the function call a = " << input << "\n";
+}
