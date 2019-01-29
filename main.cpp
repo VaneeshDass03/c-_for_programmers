34a24f471936a1adc8cc6759d549df0a57c11e4c
Vaneesh Dass <vaneesh.dass03@gmail.com>
2019-01-29 15:25:46 +0530

    c++ class basic example & syntax

--- main.cpp
+++ main.cpp
@@ -1,47 +1,88 @@
-/*Goal: Learn to pass arrays to functions*/
+/*Goal: Practice using a class*/
 
 #include<iostream>
-#include<iomanip>
 
-//Pass the array as a pointer
-void arrayAsPointer(int *array, int size);
+using namespace std;
 
-//Pass the array as a sized array
-void arraySized(int array[3], int size);
 
-//Pass the array as an unsized array
-void arrayUnSized(int array[], int size);
+class Student {
+    string name;
+    int id;
+    int gradDate;
+
+public:
+    void setName(string nameIn);
+
+    void setId(int idIn);
+
+    void setGradDate(int dateIn);
+
+    string getName();
+
+    int getId();
+
+    int getGradDate();
+
+    void print();
+};
+
+void Student::setName(string nameIn) {
+    name = nameIn;
+}
+
+void Student::setId(int idIn) {
+    id = idIn;
+}
+
+void Student::setGradDate(int gradDateIn) {
+    gradDate = gradDateIn;
+}
+
+void Student::print() {
+    cout << name << " " << id << " " << gradDate;
+}
+
+string Student::getName() {
+    return name;
+}
+
+int Student::getId() {
+    return id;
+}
+
+int Student::getGradDate() {
+    return gradDate;
+}
 
 int main() {
-    const int size = 3;
-    int array[size] = {33, 66, 99};
-    //We are passing a pointer or reference to the array
-    //so we will not know the size of the array
-    //We have to pass the size to the function as well
-    arrayAsPointer(array, size);
-    arraySized(array, size);
-    arrayUnSized(array, size);
+    int integer1;
+    float float1;
+    Student student1;
+
+    integer1 = 4; //assign a value to integer1
+    float1 = 4.333; //assign a value to float1
+
+    student1.setName("Catherine Gamboa"); //assign a value to the student name
+    student1.setId(54345); //assign a value to the student id number
+    student1.setGradDate(2017); //assign a value to the student grad date
+
+    //Let's print the values of our variables
+    cout << "integer1 = " << integer1 << "\n";
+    cout << "float1 = " << float1 << "\n\n";
+
+    //There are two ways we can print the values of our class:
+    //The first is to call the print function we created.
+    cout << "Using the Student::print function\n";
+    cout << "Student1 = ";
+    student1.print();
+    cout << "\n\n";
+
+    //The second is to access each member of the class using the get functions
+    cout << "Using the student access functions\n";
+    cout << "Student1 name = " << student1.getName() << "\n";
+    cout << "Student1 ID = " << student1.getId() << "\n";
+    cout << "Student1 Grad Date = " << student1.getGradDate() << "\n";
+
+
     return 0;
 }
-
-void arrayAsPointer(int *array, int size) {
-    std::cout << std::setw(5);
-    for (int i = 0; i < size; i++)
-        std::cout << array[i] << " ";
-    std::cout << "\n";
-}
-
-void arraySized(int array[3], int size) {
-    std::cout << std::setw(5);
-    for (int i = 0; i < size; i++)
-        std::cout << array[i] << " ";
-    std::cout << "\n";
-}
-
-void arrayUnSized(int array[], int size) {
-    std::cout << std::setw(5);
-    for (int i = 0; i < size; i++)
-        std::cout << array[i] << " ";
-    std::cout << "\n";
-}
-
