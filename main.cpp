7797db4f4874ba67b1a4e57e06c89f4593c7a40e
Vaneesh Dass <vaneesh.dass03@gmail.com>
2019-02-01 12:25:38 +0530

    c++ templates(generic) example with multiple variable types.

--- main.cpp
+++ main.cpp
@@ -1,34 +1,26 @@
-#include<iostream>
+/*Goal: learn to use templates with multiple variable types.
+*/
 
+#include<iostream>
 using namespace std;
 
-//Our generic function
-template<typename T>
-//tell the compiler we are using a template
-T findSmaller(T input1, T input2);
+template<typename T, typename U>
+T getBigger(T input1, U input2);
+
 
 int main() {
-    int a = 54;
-    int b = 89;
-    float f1 = 7.8;
-    float f2 = 9.1;
-    char c1 = 'f';
-    char c2 = 'h';
-    string s1 = "Hello";
-    string s2 = "Bots are fun";
+    int a = 5;
+    float b = 6.334;
+    int bigger;
+    cout << "Between " << a << " and " << b << " " << getBigger(a, b) << " is bigger.\n";
 
-    //Wow! We can use one function for different variable types
-    cout << "\nIntegers compared: " << findSmaller(a, b);
-    cout << "\nFloats compared: " << findSmaller(f1, f2);
-    cout << "\nChars compared: " << findSmaller(c1, c2);
-    cout << "\nStrings compared: " << findSmaller(s1, s2);
+    cout << "Between " << a << " and " << b << " " << getBigger(b, a) << " is bigger.\n";
     return 0;
 }
 
-template<typename T>
-T findSmaller(T input1, T input2) {
-    if (input1 < input2)
+template<typename T, typename U>
+T getBigger(T input1, U input2) {
+    if (input1 > input2)
         return input1;
-    else
-        return input2;
+    return input2;
 }
